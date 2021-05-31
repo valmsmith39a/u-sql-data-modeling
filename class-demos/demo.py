008a6ddb40ff707e40f03531d4053483e7b0d39b
valmsmith39a <valmsmith39a@gmail.com>
2021-05-31 16:16:41 -0700

    psycopg2, basic usage

---
+++ class-demos/demo.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+connection = psycopg2.connect("dbname=example")
+
+cursor = connection.cursor()
+
+cursor.execute(
+    """
+    CREATE TABLE table3 (
+        id INTEGER PRIMARY KEY,
+        completed BOOLEAN NOT NULL DEFAULT False
+    );
+"""
+)
+
+cursor.execute("INSERT INTO table3 (id, completed) VALUES (1, true);")
+
+connection.commit()
+
+connection.close()
+cursor.close()
