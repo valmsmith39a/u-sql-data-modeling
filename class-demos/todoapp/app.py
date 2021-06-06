7c4faa5b5b5f9a2d007efdf62455a1b511a10d8f
valmsmith39a <valmsmith39a@gmail.com>
2021-06-06 14:01:59 -0700

    todo app and related notes

---
+++ class-demos/todoapp/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__) # create an application named after our file (app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://georgewee@localhost:5432/todoapp'
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, impary_key=True)
+  description = db.Column(db.String(), nullable=False)
+  
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
+
+@app.route('/')
+def index(): 
+  return render_template('index.html', data=Todo.query.all())k
+
+ 
