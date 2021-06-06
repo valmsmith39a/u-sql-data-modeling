206861b724d5667bc520bb1e55558c7b3936f4f9
valmsmith39a <valmsmith39a@gmail.com>
2021-06-06 01:05:08 -0700

    notes on python interactive, SQLAlchemy query object functions

--- hello-app-flask/flask_hello_app.py
+++ hello-app-flask/flask_hello_app.py
@@ -3,13 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://georgewee@localhost:5432/example'
-
+app.config['SQLALCHEMY_TRACK_MODIFCATIONS'] = False
 db = SQLAlchemy(app)
 
 class Person(db.Model):
   __tablename__ = 'persons'
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(), nullable=False)
+  
+  def __repr__(self):
+    return f'<Person ID: {self.id}, name: {self.name}>'
   
 db.create_all()
 
