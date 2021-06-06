0f6124a940c42822ecf34bd87d713ae5bc70f15a
valmsmith39a <valmsmith39a@gmail.com>
2021-06-06 11:33:13 -0700

    notes: SQLAlchemy lifecycle

--- hello-app-flask/flask_hello_app.py
+++ hello-app-flask/flask_hello_app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://georgewee@localhost:5432/example'
-app.config['SQLALCHEMY_TRACK_MODIFCATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Person(db.Model):
