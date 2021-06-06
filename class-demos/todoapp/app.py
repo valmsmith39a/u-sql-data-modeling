f75e6b65fbd84612a412cbe2d6be950cabf85538
valmsmith39a <valmsmith39a@gmail.com>
2021-06-06 14:16:43 -0700

    debug flask app and add requirements

--- class-demos/todoapp/app.py
+++ class-demos/todoapp/app.py
@@ -1,23 +1,24 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__) # create an application named after our file (app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://georgewee@localhost:5432/todoapp'
+app = Flask(__name__)  # create an application named after our file (app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://georgewee@localhost:5432/todoapp"
 
 db = SQLAlchemy(app)
 
+
 class Todo(db.Model):
-  __tablename__ = 'todos'
-  id = db.Column(db.Integer, impary_key=True)
-  description = db.Column(db.String(), nullable=False)
-  
-  def __repr__(self):
-    return f'<Todo {self.id} {self.description}>'
+    __tablename__ = "todos"
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f"<Todo {self.id} {self.description}>"
+
 
 db.create_all()
 
-@app.route('/')
-def index(): 
-  return render_template('index.html', data=Todo.query.all())k
 
- 
+@app.route("/")
+def index():
+    return render_template("index.html", data=Todo.query.all())
