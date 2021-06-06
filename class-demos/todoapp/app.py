f8fde086455b67f80f20bf2ae422f8d7f3a885a3
valmsmith39a <valmsmith39a@gmail.com>
2021-06-06 16:51:24 -0700

    error handling

--- class-demos/todoapp/app.py
+++ class-demos/todoapp/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask import Flask, render_template, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys 
 
 app = Flask(__name__)  # create an application named after our file (app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://georgewee@localhost:5432/todoapp"
@@ -20,15 +21,22 @@ db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-  print('inside create todos', request.get_json())
-  description = request.json['description']
-  todo = Todo(description=description)
-  db.session.add(todo)
-  db.session.commit()
-  return jsonify({
-    'description': todo.description
-  }) 
-
+  error = False
+  body = {}
+  try:
+    description = request.json['description']
+    todo = Todo(description2=description)
+    db.session.add(todo)
+    db.session.commit()
+    body['description'] = todo.description
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if not error:
+      return jsonify(body)
 
 @app.route("/")
 def index():
