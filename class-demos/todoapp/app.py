bef65c7b6622e35e4d9e1bfb1962ba9144d13b76
valmsmith39a <valmsmith39a@gmail.com>
2021-06-07 21:55:58 -0700

    update a checkbox in a todo item

--- class-demos/todoapp/app.py
+++ class-demos/todoapp/app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, jsonify, abort
+from flask import Flask, render_template, request, jsonify, abort, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-import sys 
+import sys
 
 app = Flask(__name__)  # create an application named after our file (app)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://georgewee@localhost:5432/todoapp"
+app.config[
+    "SQLALCHEMY_DATABASE_URI"] = "postgresql://georgewee@localhost:5432/todoapp"
 
 db = SQLAlchemy(app)
 
@@ -11,7 +12,8 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     __tablename__ = "todos"
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=True)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f"<Todo {self.id} {self.description}>"
@@ -19,28 +21,42 @@ class Todo(db.Model):
 
 db.create_all()
 
+
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-  error = False
-  body = {}
-  try:
-    description = request.json['description']
-    todo = Todo(description2=description)
-    db.session.add(todo)
-    db.session.commit()
-    body['description'] = todo.description
-  except:
-    error = True
-    db.session.rollback()
-    print(sys.exc_info())
-  finally:
-    db.session.close()
-  if not error:
-      return jsonify(body)
+    error = False
+    body = {}
+    try:
+        description = request.json['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if not error:
+        return jsonify(body)
+
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    print('inside completed todos')
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
 
 @app.route("/")
 def index():
-    print('inside root')
-    return render_template("index.html", data=Todo.query.all())
-
-
+    return render_template("index.html", data=Todo.query.order_by('id').all())
