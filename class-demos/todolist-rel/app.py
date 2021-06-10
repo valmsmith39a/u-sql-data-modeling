aaabe446eb3633da832c54da6c45119ae0a57c88
valmsmith39a <valmsmith39a@gmail.com>
2021-06-09 21:17:58 -0700

    crud with lists and todos

--- class-demos/todolist-rel/app.py
+++ class-demos/todolist-rel/app.py
@@ -53,7 +53,10 @@ def create_todo():
 
 @app.route("/lists/<list_id>")
 def get_list_todos(list_id):
-    return render_template("index.html", data=Todo.query.filter_by(list_id=list_id).all())
+    return render_template("index.html", 
+		lists=TodoList.query.all(),
+		active_list=TodoList.query.get(list_id), 
+		todos=Todo.query.filter_by(list_id=list_id).all())
 
 @app.route('/')
 def index():
